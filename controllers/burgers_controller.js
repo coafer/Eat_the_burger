551fd456939ed4de6f59a2fde24a2069c597360c
coafer <mac@macs-MacBook-Pro.local>
2018-07-01 12:12:06 -0400

    Server sucessfully connected

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,11 @@
 var express = require("express");
-
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger");
+// // Import the model (burger.js) to use its database functions.
+// var burger = require("../models/burger");
+
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 // Set port and Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8060;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//To use methods like Put and update
+app.use(methodOverride("_method"));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -20,6 +24,11 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
