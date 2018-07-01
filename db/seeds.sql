f6372cf78ba55246fb04e20c09cb83babdbf1d92
coafer <mac@macs-MacBook-Pro.local>
2018-06-30 21:33:36 -0400

    DB and seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (name, tocart)
+VALUES
+    ('Cheese burger', false);
+INSERT INTO burgers
+    (name, tocart)
+VALUES
+    ('Hamburger meety', false);
+INSERT INTO burgers
+    (name, tocart)
+VALUES
+    ('Fish burger', false);
