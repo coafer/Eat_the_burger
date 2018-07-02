c1cac7c1a87782f78bf6073af45b12082e1f3886
coafer <mac@macs-MacBook-Pro.local>
2018-07-01 21:57:55 -0400

    Manage to obtain data from DB for route get all

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
-// // Import the model (burger.js) to use its database functions.
-// var burger = require("../models/burger");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger");
 
+// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  res.render("index");
+  burger.all(function(burger_data) {
+    var hbsObject = {
+      burgers: burger_data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
+
+// // Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+//     cat.all(function(data) {
+//       var hbsObject = {
+//         cats: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+//   });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,18 @@
 var orm = require("../config/orm.js");
 
-var burgers = {
-  all: function(cb) {
-    orm.all("burger", function(res) {
+var burger = {
+  _all: function(cb) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burger", cols, vals, function(res) {
-      cb(res);
-    });
+  get all() {
+    return this._all;
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burger", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-
-  delete: function(condition, cb) {
-    orm.delete("burger", condition, function(res) {
-      cb(res);
-    });
+  set all(value) {
+    this._all = value;
   }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burgers;
+module.exports = burger;
